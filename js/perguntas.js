50773f4a9c81c26a27d5f442f8a90457fdb93309
davibi2006 <110997189+davibi2006@users.noreply.github.com>
2024-08-26 12:11:45 -0300

    daviana

---
+++ js/perguntas.js
@@ -0,0 +1,128 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que você sai de casa e percebe que é a única pessoa no mundo, qual é a sua reação inicial?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "Você ficou apreensivo com a ideia de estar sozinho em um mundo vazio.",
+                    "Você está preocupado com os desafios de viver sem a presença de outras pessoas e como isso afetará sua vida cotidiana."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é emocionante!",
+                afirmacao: [
+                    "Você sente uma excitação com a possibilidade de explorar o mundo sem limitações.",
+                    "Você está animado com a oportunidade de experimentar a liberdade total e fazer o que quiser."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Agora que você percebeu que é a única pessoa no mundo, você precisa decidir como vai organizar sua nova rotina. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Cria um plano detalhado para explorar e catalogar os recursos disponíveis, como alimentos e água, para garantir sua sobrevivência.",
+                afirmacao: [
+                    "Você conseguiu mapear os recursos essenciais e está preparado para viver de forma autossuficiente.",
+                    "Você percebeu que a organização e a preparação são essenciais para garantir uma vida confortável e segura sozinho."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Decide se dedicar ao que sempre quis fazer, como viajar para lugares que sempre sonhou conhecer, sem se preocupar com a sobrevivência imediata.",
+                afirmacao: [
+                    "Você encontrou uma nova paixão e está aproveitando a liberdade para explorar e vivenciar experiências que antes eram apenas sonhos.",
+                    "Você percebeu que a exploração pode ser uma forma de lidar com a solidão e aproveitar o tempo disponível da melhor forma."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Você começa a explorar as cidades vazias e encontra um centro de pesquisa abandonado. Como você decide utilizá-lo?",
+        alternativas: [
+            {
+                texto: "Decide investigar os equipamentos e tentar entender como funcionam para aprender mais e possivelmente encontrar soluções para qualquer desafio futuro.",
+                afirmacao: [
+                    "Você encontrou novos conhecimentos e habilidades que podem ser úteis para sua sobrevivência e adaptação.",
+                    "Você sente que explorar e aprender é essencial para se adaptar ao novo mundo em que está."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Opta por usar o centro de pesquisa como um local para se abrigar e se concentrar em atividades recreativas para se manter entretido.",
+                afirmacao: [
+                    "Você conseguiu criar um ambiente confortável e estimulante para lidar com a solidão.",
+                    "Você percebeu que é importante manter-se mentalmente ativo e entretido para manter o bem-estar emocional."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Enquanto explora a cidade, você se depara com uma vasta coleção de livros e mídias em uma biblioteca. O que você faz com isso?",
+        alternativas: [
+            {
+                texto: "Decide ler e estudar os livros e mídias para aproveitar o conhecimento disponível e enriquecer seu tempo.",
+                afirmacao: [
+                    "Você descobriu novos interesses e aprofundou seu conhecimento, o que ajuda a manter a mente ativa e engajada.",
+                    "Você percebeu que o acesso ao conhecimento é uma forma valiosa de passar o tempo e se preparar para possíveis desafios."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Opta por usar os livros e mídias para criar um projeto pessoal, como escrever um livro ou produzir uma obra de arte.",
+                afirmacao: [
+                    "Você encontrou uma forma criativa de expressar seus pensamentos e sentimentos durante a solidão.",
+                    "Você percebeu que criar algo pessoal pode ser uma forma significativa de passar o tempo e dar sentido à sua nova vida."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Após algum tempo sozinho, você encontra um antigo diário em uma casa abandonada. O que você faz com ele?",
+        alternativas: [
+            {
+                texto: "Decide ler o diário para entender mais sobre a vida das pessoas que estavam ali antes e refletir sobre suas próprias experiências.",
+                afirmacao: [
+                    "Você encontrou uma conexão emocional com o passado e isso ajudou a entender melhor sua própria situação.",
+                    "Você percebeu que explorar o passado pode fornecer insights valiosos e ajudar a lidar com a solidão."
+                ],
+                proxima: 9,
+            },
+            {
+                texto: "Opta por usar o diário como uma inspiração para começar seu próprio diário e registrar suas próprias experiências e sentimentos.",
+                afirmacao: [
+                    "Você começou a documentar suas próprias experiências e pensamentos, o que ajuda a processar suas emoções e criar um legado pessoal.",
+                    "Você percebeu que manter um diário pode ser uma forma importante de autoexpressão e reflexão durante a solidão."
+                ],
+                proxima: 10,
+            },
+        ]
+    },
+    {
+        enunciado: "Depois de algum tempo sozinho, você decide criar uma obra de arte ou um projeto. Qual é o foco do seu projeto?",
+        alternativas: [
+            {
+                texto: "Decide criar uma obra de arte que retrata a beleza do mundo vazio e sua nova perspectiva sobre a vida.",
+                afirmacao: [
+                    "Você encontrou uma forma de expressar suas emoções e pensamentos através da arte, capturando a beleza e a complexidade de sua nova realidade.",
+                    "Você percebeu que a arte pode ser uma forma poderosa de comunicação e reflexão pessoal."
+                ],
+                proxima: 11,
+            },
+            {
+                texto: "Opta por desenvolver um projeto prático, como construir um abrigo ou um sistema para coletar água, para melhorar sua vida cotidiana.",
+                afirmacao: [
+                    "Você conseguiu criar soluções práticas para melhorar sua qualidade de vida e se adaptar ao novo ambiente.",
+                    "Você percebeu que projetos práticos são essenciais para garantir sua sobrevivência e bem-estar em um mundo vazio."
+                ],
+                proxima: 12,
+            },
+        ]
+    }
+];
